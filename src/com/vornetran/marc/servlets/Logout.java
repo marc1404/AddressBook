e060fe65b3f9433b514c886d1f9cbe39f8550dbe
Marc <marc.vornetran@gmail.com>
2015-05-11 12:10:01 +0200

    Fix my stupidness

---
+++ src/com/vornetran/marc/servlets/Logout.java
@@ -0,0 +1,23 @@
+package com.vornetran.marc.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Marc on 11.05.2015.
+ */
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().invalidate();
+        response.sendRedirect("/");
+    }
+
+}
